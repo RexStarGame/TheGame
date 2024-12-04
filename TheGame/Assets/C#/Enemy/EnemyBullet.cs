cb2d7b5d46f2e6c2bf91a1266ec1f1070b730d4c
Patrick <63421519+RexStarGame@users.noreply.github.com>
2024-12-04 13:14:39 +0100

    update
    fixed enemies

--- TheGame/Assets/C#/Enemy/EnemyBullet.cs
+++ TheGame/Assets/C#/Enemy/EnemyBullet.cs
@@ -1,11 +1,19 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class EnemyBullet : MonoBehaviour
 {
-    public float speed = 10f; // Kuglens hastighed
-    public int damage = 10; // Hvor meget skade kuglen giver
-    public float lifetime = 5f; // Hvor længe kuglen lever, før den destrueres
+    [SerializeField] private float speed = 10f; // Kuglens hastighed
+    [SerializeField] private int damage = 10; // Hvor meget skade kuglen giver
+    [SerializeField] private float lifetime = 5f; // Hvor længe kuglen lever, før den destrueres
+
     private PlayerHealth health;
+    private Vector3 direction;
+    public void intallized(Vector3 firedirection)
+    {
+        direction = firedirection.normalized;
+    }
+
     private void Start()
     {
         // Ødelæg kuglen efter en vis tid, hvis den ikke rammer noget
@@ -15,17 +23,14 @@ public class EnemyBullet : MonoBehaviour
     private void Update()
     {
         // Flyt kuglen fremad baseret på dens hastighed
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(direction * speed * Time.deltaTime);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         // Tjek om kuglen rammer spilleren
         if (collision.gameObject.layer != 3)
-        {
-
-     
-            
+        {  
             if(collision.CompareTag("Player"))
             {
               health.TakeDamage(5);
--- TheGame/Assets/C#/Enemy/EnemyShooting.cs
+++ TheGame/Assets/C#/Enemy/EnemyShooting.cs
@@ -31,10 +31,10 @@ public class EnemyShooting : MonoBehaviour
     bool IsPlayerInRange()
     {
         // Tegn Raycast i Editor
-        Debug.DrawRay(firePoint.position, firePoint.right * detectionRange, Color.red);
+        Debug.DrawRay(firePoint.position, firePoint.right * detectionRange * transform.localScale.x, Color.red);
 
         // Udfør Raycast med korrekt LayerMask
-        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right, detectionRange, playerLayer);
+        RaycastHit2D hit = Physics2D.Raycast(firePoint.position, firePoint.right, detectionRange * transform.localScale.x, playerLayer);
 
         if (hit.collider != null && hit.collider.CompareTag("Player"))
         {
@@ -53,6 +53,10 @@ public class EnemyShooting : MonoBehaviour
         if (enemyBullet != null && firePoint != null)
         {
             GameObject bullet = Instantiate(enemyBullet, firePoint.position, firePoint.rotation);
+
+            Vector3 fireDirection = transform.localScale.x > 0 ? Vector3.right: Vector3.left;
+
+            bullet.GetComponent<EnemyBullet>().intallized(fireDirection);
 
             Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
             if (rb != null)
